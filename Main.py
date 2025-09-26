1b04ddebafffe9b1a52cb753b4ab285615d99266
IvanEstudios <ivanEstudios@gmail.com>
2025-09-26 14:55:55 +0200

    manejar teclado y raton

---
+++ Main.py
@@ -0,0 +1,26 @@
+import keyboard, mouse
+
+def on_press(event):
+    print(event.name)
+
+    match event.name:
+
+        case "volume up":
+            mouse.move(100,100)
+        case "volume down":
+            print("bajando volumen")
+
+        case "next track":
+            keyboard.send("flecha derecha")
+        case "previous track":
+            keyboard.send("flecha izquierda")
+
+        case "play/pause media":
+            print("play/pause")
+
+
+
+
+keyboard.on_press(on_press)
+
+keyboard.wait('esc')
